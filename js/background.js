0d1e5775baf2d2ef9e4e65da07f2c2776a161145
minjae1025 <kjt081025@gmail.com>
2025-03-20 14:41:50 +0900

    어제 오늘 내일 추가

---
+++ js/background.js
@@ -0,0 +1,23 @@
+function background(){
+    let now = new Date().getHours();
+    console.log(now);
+    let imageContainer = document.getElementById("moon");
+    let img = document.createElement("img");
+    img.style.width = "3vw";
+    let time_box = document.getElementById("blue_box");
+    let box = document
+
+    if(now >= 6 && now < 18){
+        document.body.style.background = "linear-gradient(to bottom, rgba(168, 251, 255, 0.48), #FFFFFF)";
+        img.src = "../img/day.png";
+        time_box.style.backgroundColor = "#7ABFFF";
+        time_box.style.color = "#000000";
+    } else{
+        document.body.style.background = "linear-gradient(to bottom, #223056, #FFFFFF)";
+        time_box.style.backgroundColor = "#2C4982";
+        time_box.style.color = "#ffffff"
+        img.src = "../img/night.png";
+    }
+    document.body.style.backgroundAttachment = "fixed";
+    imageContainer.appendChild(img);
+}
--- js/food_script.js
+++ js/food_script.js
@@ -1,10 +1,10 @@
-function getMealInfo() {
+function getMealInfo(date) {
     const API_KEY = "12223b6763eb4851acee4c8f03d4dba8";
     const URL = "https://open.neis.go.kr/hub/mealServiceDietInfo";
     const ATPT_OFCDC_SC_CODE = "B10";   //서울 특별시 교육청
     const SD_SCHUL_CODE = "7011569";
     const TYPE = "json";
-    const dateData = (document.getElementById("dateInput").value).replace(/-/g, '');
+    const dateData = date;
     const api_url = `https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${SD_SCHUL_CODE}&KEY=${API_KEY}&MLSV_YMD=${dateData}&Type=${TYPE}`;
     console.log(dateData);
   
