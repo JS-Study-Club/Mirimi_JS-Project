594ed0cf34b51c0ae2886252e9fdff99fbe19968
minjae1025 <kjt081025@gmail.com>
2025-03-24 23:06:41 +0900

    시간표 js, css 수정

--- js/timetable.js
+++ js/timetable.js
@@ -1,6 +1,6 @@
 function getTimeTable(grade, class_nm) {
     const API_KEY = "90de860d4ab54f7eb75640bf431149a4";
-    const URL = "https://open.neis.go.kr/hub/hisTimetable";
+    const URL = "https://open.neis.go.kr/hub/hisTimetable?";
     const ATPT_OFCDC_SC_CODE = "B10";   //서울 특별시 교육청
     const SD_SCHUL_CODE = "7011569";
     const TYPE = "json";
@@ -9,13 +9,47 @@ function getTimeTable(grade, class_nm) {
     const GRADE = grade;
     const CLASS_NM = class_nm;
     var ALL_TI_YMD = mondayDate();
+    var timeTableList = new Array(7);
+
+    for (i = 0; i < 7; i++) {
+        timeTableList[i] = new Array(5);
+    }
 
     function fetchTimetable(dayCount) {
-        if (dayCount > 4) return;  // 금요일까지 요청 완료하면 종료
+        const IDTable = document.getElementById("timeTable");
+
+        if (dayCount > 4) {
+            // IDTable.innerHTML = "";
+            // let headerRow = "";
+            // IDTable.innerHTML += headerRow;
+            //html로 함
+
+            // 시간표 내용 채우기 (가로 방향)
+            for (let col = 0; col < 7; col++) {
+                let rowHTML = `<tr><th>${col + 1}</th>`; // 첫 번째 열에 교시 번호 추가
+                for (let row = 0; row < 5; row++) { // 요일별 데이터 추가
+                    if (typeof timeTableList[col][row] === 'undefined') {
+                        rowHTML += "<td></td>"; // 빈 칸
+                    } else {
+                        let subject = timeTableList[col][row].replace(/\*/g, ""); // "*" 제거
+                        // console.log(subject);
+                        rowHTML += `<td>${subject}</td>`;
+                    }
+                }
+                rowHTML += "</tr>";
+                IDTable.innerHTML += rowHTML;
+            }
+            const weekday = Date.prototype.getDay();
+            console.log(weekday);
+            //if ()
+            return; // 금요일까지 요청 완료하면 종료
+        }
 
         console.log(ALL_TI_YMD);
 
         let api_url = `https://open.neis.go.kr/hub/hisTimetable?KEY=${API_KEY}&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${SD_SCHUL_CODE}&AY=${AY}&SEM=${SEM}&ALL_TI_YMD=${ALL_TI_YMD}&GRADE=${GRADE}&CLASS_NM=${CLASS_NM}&Type=${TYPE}`;
+
+        var i = 0;
 
         fetch(api_url)
             .then(response => {
@@ -25,13 +59,20 @@ function getTimeTable(grade, class_nm) {
                 return response.json();
             })
             .then(data => {
-                let TimeTable = data.hisTimetable[1].row;
+                let Table = data.hisTimetable[1].row;
 
-                console.log(TimeTable);
-
-                TimeTable.forEach(element => {
-                    console.log(element.ITRT_CNTNT);
-                });
+                // console.log(Table);
+                var end = 0;
+                if (dayCount == 0 || dayCount == 4) {
+                    end = 6;
+                }
+                else {
+                    end = 7;
+                }
+                for (i = 0; i < end; i++) {
+                    // console.log(Table[i].ITRT_CNTNT);
+                    timeTableList[i][dayCount] = Table[i].ITRT_CNTNT;
+                }
 
                 // 다음 요일로 이동
                 ALL_TI_YMD = dateplus(ALL_TI_YMD);
@@ -43,6 +84,7 @@ function getTimeTable(grade, class_nm) {
     }
 
     fetchTimetable(0);  // 첫 번째 요청 시작 (월요일)
+
 
 
 }
