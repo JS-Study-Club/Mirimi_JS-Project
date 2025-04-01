13aa2a104bb71a65deaa1009e86cecfa325a1e9a
minjae1025 <kjt081025@gmail.com>
2025-04-01 22:45:38 +0900

    [feat] 정규표현식을 활용해 체크 후, 학년 반 분리

--- js/regist_check.js
+++ js/regist_check.js
@@ -1,14 +1,26 @@
+const regex = /^[1-3][1-6][0-9]{2}$/;
+
 function checkId() {
     var left = Math.ceil((window.screen.width - 500) / 2);
     var top = Math.ceil((window.screen.height - 300) / 2);
     var userid = document.getElementById("user-id").value;
     var win = window.open("regist_proc.php?userid=" + userid, "Idcheck", `width=500, height=300, top=${top}, left=${left}`);
-}   
+}
 
 const sendit = () => {
     const user_grade = document.regist_form.user_grade.value;
     const user_name = document.regist_form.user_name.value;
     const user_id = document.regist_form.user_id.value;
     const user_pw = document.regist_form.user_pw.value;
-    // if ()
+
+    if (regex.test(user_grade)) {
+        let gradeNum = user_grade.charAt(0); //학년
+        let classNum = user_grade.charAt(1); //반
+
+        console.log(`학년: ${gradeNum}, 반: ${classNum}`);
+    } else {
+        console.log("올바르지 않은 학번입니다.");
+    }
+
+
 }
