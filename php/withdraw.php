055cc16895545aa95c595368ceead0c8ec22fdb9
minjae1025 <kjt081025@gmail.com>
2025-04-03 22:42:55 +0900

    [fix] 탈퇴해도 남았던 세션 오류

--- php/withdraw.php
+++ php/withdraw.php
@@ -9,7 +9,24 @@ $result = mysqli_query($db_conn, $sql);
 if ($result === false) {
     echo "문제가 생겼습니다. 관리자에게 문의 바랍니다.";
     echo mysqli_error($db_conn);
-} else { ?>
+} else {
+    session_unset();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params["path"],
+            $params["domain"],
+            $params["secure"],
+            $params["httponly"]
+        );
+    }
+    session_destroy();
+    unset($_SESSION['name']);
+    ?>
+    ?>
     <script>
         alert("탈퇴가 완료되었습니다.");
         location.href = "index.php";
@@ -18,4 +35,5 @@ if ($result === false) {
     <?php
 }
 
-?>
+in
+    ?>
