889457a589569a94304493715bc74a71e373f56f
minjae1025 <kjt081025@gmail.com>
2025-04-14 18:56:49 +0900

    [fix] 회원가입 오류 수정

--- php/regist_proc.php
+++ php/regist_proc.php
@@ -7,13 +7,11 @@ $id = $_POST["user_id"];
 $pw = $_POST["user_pw"];
 $name = $_POST["user_name"];
 $grade = substr($user_grade, 0, 1);
-echo $grade;
+//echo $grade;
 $class = substr($user_grade, 1, 1);
-echo $class;
+//echo $class;
 $sql = "INSERT INTO mirimi_users (user_id, user_pw, user_name, user_grade, user_class) VALUES ('$id', '$pw', '$name', '$grade', '$class');";
 $result = mysqli_query($db_conn, $sql);
-/* $result = $db_conn -> query($sql);*/
-// 입력이 됐으면 결과가 1
 
 if ($result === false) { /* === 이면 자료형까지 일치하는지 확인 */
     echo "저장에 문제가 생겼습니다. 관리자에게 문의 바랍니다.";
